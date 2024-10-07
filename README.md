23f9eabe59a2054782e20c7cdb53c5147fd9f479
cheoljun99 <cheoljun99@naver.com>
2024-10-07 16:13:53 +0900

    [chore]: 프로젝트 디렉토리 구조 변경 24.10.07

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # STM32_Study
+
+STM32 펌웨어 공부 및 구현 repository입니다. 
---
+++ STM32_Study_by_cheoljun99/STM32F407VGT6_CAN_Sniffer_RTOS/Debug/COBSLib/Src/cobs.d
@@ -0,0 +1,3 @@
+COBSLib/Src/cobs.o: ../COBSLib/Src/cobs.c \
+ C:/Users/cheoljun99/Desktop/STM32_reference/STM32_Study/STM32F407VGT6_CAN_Sniffer_RTOS/COBSLib/Inc/cobs.h
+C:/Users/cheoljun99/Desktop/STM32_reference/STM32_Study/STM32F407VGT6_CAN_Sniffer_RTOS/COBSLib/Inc/cobs.h:
